7a316daceeef5f5807858875283289711341ecce
Giordan Garcia <giordangarcia12@gmail.com>
2025-10-29 15:46:41 -0300

    create form consult

--- src/app/consult/consult.component.ts
+++ src/app/consult/consult.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { ClientService } from '../client.service';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-consult',
-  imports: [],
+  imports: [MatInputModule, MatCardModule, FlexLayoutModule, MatIconModule, FormsModule, MatTableModule, MatButtonModule],
   templateUrl: './consult.component.html',
   styleUrl: './consult.component.scss'
 })
 export class ConsultComponent {
+
+  constructor(
+    private clientService: ClientService
+  ){}
+
+
 
 }
