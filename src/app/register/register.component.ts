899259c48c06de576990a7553feee6fe34ec8bb6
Giordan Garcia <giordangarcia12@gmail.com>
2025-11-13 00:05:42 -0300

    add mask to fields

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, inject } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
@@ -6,11 +6,13 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Client } from'./client';
 import { ClientService } from '../client.service';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 
 @Component({
   selector: 'app-register',
@@ -22,14 +24,19 @@ import { Router } from '@angular/router';
     MatInputModule,
     MatIconModule,
     MatButtonModule,
-    CommonModule
+    CommonModule,
+    NgxMaskDirective
   ],
+  providers: [provideNgxMask()],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  @ViewChild('clientForm') clientForm: any;
+
   client: Client = Client.newClient();
   editing: boolean = false;
+  snackBar = inject(MatSnackBar);
 
   constructor(private clientService: ClientService, private activatedRoute: ActivatedRoute, private router: Router) {}
 
@@ -37,10 +44,18 @@ export class RegisterComponent {
     if (!this.editing){
       this.clientService.addClient(this.client);
       this.client = Client.newClient();
+      this.showMessage("Cliente cadastrado com sucesso!");
     }else{
       this.clientService.edit(this.client);
       this.router.navigate(['/consult']);
+      this.showMessage("Cliente atualizado com sucesso!");
+
     }
+  }
+
+  clearForm() {
+    this.client = Client.newClient();
+    this.clientForm.resetForm();
   }
 
   ngOnInit(): void {
@@ -54,6 +69,10 @@ export class RegisterComponent {
       }
     });
     console.log(this.client);
+  }
+
+  showMessage(message: string) {
+    this.snackBar.open(message, "OK", { duration: 3000 });
   }
 
 }
